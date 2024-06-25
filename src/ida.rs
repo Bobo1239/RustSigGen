717221de1a7291ba23c1e35d23825ace9176e848
Boris-Chengbiao Zhou <bobo1239@web.de>
2024-06-25 19:14:51 +0200

    Make IDA signatures a bit nicer (signature library name; zipsig)

--- src/ida.rs
+++ src/ida.rs
@@ -18,30 +18,35 @@ pub fn generate_signatures(
 ) -> Result<PathBuf> {
     info!("Generating IDA F.L.I.R.T. signatures...");
 
-    // TODO: Fix signature library name (atm IDA outputs `Using FLIRT signature: Unnamed sample library`)
     let bin_path = flair_path.join("bin/linux");
     if !bin_path.exists() {
         bail!("FLAIR directory doesn't seem correct; failed to find `bin/linux`")
     }
 
+    let pat_path = tmp_dir.path().join("std.pat");
+    let sig_path = tmp_dir.path().join("std.sig");
+    let exc_path = tmp_dir.path().join("std.exc");
+
     let status = Command::new(bin_path.join("pelf"))
         .arg(tmp_dir.path().join("*.o"))
-        .arg(tmp_dir.path().join("std.pat"))
+        .arg(&pat_path)
         .stderr(Stdio::null())
         .status()?;
     ensure!(status.success(), "pelf failed; non-zero exit code");
 
+    let mut sigmake_command = Command::new(bin_path.join("sigmake"));
+    sigmake_command
+        .arg(format!("-n{}", release_manifest.release.path_name()))
+        .arg(&pat_path)
+        .arg(&sig_path);
+
     // Not checking for exit code here since it will be non-zero on collisions (which we expect)
-    let output = Command::new(bin_path.join("sigmake"))
-        .arg(tmp_dir.path().join("std.pat"))
-        .arg(tmp_dir.path().join("std.sig"))
-        .output()?;
+    let output = sigmake_command.output()?;
     info!(
         "sigmake output: {}",
         str::from_utf8(&output.stderr)?.lines().next().unwrap()
     );
 
-    let exc_path = tmp_dir.path().join("std.exc");
     let exc = std::fs::read_to_string(&exc_path)?;
     // Skipping the first four lines indicates to `sigmake` that we just want to skip the
     // collisions.
@@ -49,18 +54,19 @@ pub fn generate_signatures(
     let new_exc = exc.lines().skip(4).collect::<Vec<_>>().join("\n");
     std::fs::write(&exc_path, new_exc)?;
 
-    let status = Command::new(bin_path.join("sigmake"))
-        .arg(tmp_dir.path().join("std.pat"))
-        .arg(tmp_dir.path().join("std.sig"))
-        .status()?;
+    let status = sigmake_command.status()?;
     ensure!(status.success(), "sigmake failed; non-zero exit code");
+
+    let output = Command::new(bin_path.join("zipsig"))
+        .arg(&sig_path)
+        .output()?;
+    info!("zipsig output: {}", str::from_utf8(&output.stdout)?.trim());
+    ensure!(status.success(), "zipsig failed; non-zero exit code");
 
     std::fs::create_dir_all(&out_dir)?;
     let out_path = out_dir.join(format!("{}.sig", release_manifest.release.path_name()));
-    std::fs::copy(tmp_dir.path().join("std.sig"), &out_path)?;
+    std::fs::copy(sig_path, &out_path)?;
     info!("Generated {}", out_path.display());
-
-    // TODO: zipsig?
 
     Ok(out_path)
 }
